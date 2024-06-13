ce18e2168437ab7fb22ec532d526ecf899e5d129
cojmar <cojmar@gmail.com>
2024-06-13 07:43:13 +0300

    swap

--- install.sh
+++ install.sh
@@ -26,6 +26,18 @@ echo $sep && printf "%s" "Host name (default cojarch) : " && read my_host_name &
 echo $my_host_name && echo ''
 mkfs.fat $boot_part && mkfs.ext4 -F $sys_part && sync
 mount $sys_part /mnt && mount --mkdir $boot_part /mnt/boot/efi
+echo $sep && printf "Make swap? (leave blank for yes) :" && read my_host_name && if [[ -z "$my_host_name" ]]; then 
+    # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
+    mkdir -p /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
+    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
+    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
+    chmod 600 /mnt/opt/swap/swapfile # set permissions.
+    chown root /mnt/opt/swap/swapfile
+    mkswap /mnt/opt/swap/swapfile
+    swapon /mnt/opt/swap/swapfile
+    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
+    echo "/opt/swap/swapfile    none    swap    sw    0    0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
+fi
 timedatectl set-ntp true
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
