23dba695741b9f0c263c0361999ac910e19c0f85
cojmar <cojmar@gmail.com>
2024-06-16 09:56:34 +0300

    .

--- install.sh
+++ install.sh
@@ -332,6 +332,7 @@ arch-chroot -u $my_user /mnt /bin/sh -c '
 cd /home/$my_user
 sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git 
 cd yay-bin && makepkg -si --noconfirm && cd .. && rm -rf yay-bin
+yay
 yay -Syu --noconfirm pacseek && yay -Yc --noconfirm
 '
 fi
