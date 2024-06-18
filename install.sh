e3c74e39b819244201018e6953ea46af41c99004
cojmar <cojmar@gmail.com>
2024-06-18 07:47:08 +0300

    .

--- install.sh
+++ install.sh
@@ -346,7 +346,7 @@ if [ "$my_gui_autostart" != "n" ]; then
 echo -ne '
 echo -ne "
 if [[ ! \$DISPLAY && \$XDG_VTNR -eq 1 ]]; then
-    startx > /dev/null
+    startx 2&>1
 fi
 " > /home/$my_user/.bash_profile && chown $my_user /home/$my_user/.bash_profile
 
