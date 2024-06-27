c48b928da30f6bea6a18f00d6580eff751f50270
cojmar <cojmar@gmail.com>
2024-06-27 22:16:41 +0300

    .

--- install.sh
+++ install.sh
@@ -234,8 +234,8 @@ if [ "$my_opt" = "2" ]; then
 elif [ "$my_opt" = "3" ]; then
     export my_make_swap=$my_def_swap_opt
     export my_user_autologin=y
-    export my_extra+=" brave"
-    export my_drivers=1
+    export my_extra+=" brave code"
+    export my_drivers=3
 
     echo $sep
     echo DESKTOP ENV
@@ -383,9 +383,13 @@ if [ "$my_drivers" != "0" ]; then
 fi
 
 if [ "$my_drivers" = "2" ]; then
-my_pacman+=(winetricks zenity lutris vulkan-tools)
-#WineDependencies 
-my_pacman+=(wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader)
+my_pacman+=(wine-staging winetricks zenity)
+#lutris and WineDependencies 
+my_pacman+=(lutris vulkan-tools wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader)
+fi
+
+if [ "$my_drivers" = "3" ]; then
+my_pacman+=(wine-staging winetricks zenity)
 fi
 
 
