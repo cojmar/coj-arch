e1e3400b25e4ba6eab23ee94aba3bc9d9c466fe0
cojmar <cojmar@gmail.com>
2026-03-11 10:08:21 +0200

    ..

--- install.sh
+++ install.sh
@@ -806,9 +806,6 @@ arch-chroot /mnt /bin/sh -c '
     ./clean.sh    
 '
 fi
-sync
-mkdir -p /mnt/boot/efi/EFI/BOOT
-cp /mnt/boot/efi/EFI/arch/grubx64.efi /mnt/boot/efi/EFI/BOOT/BOOTX64.EFI
 
 sync
 fastfetch
