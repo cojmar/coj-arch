519ba7f45b928da2101e759d4396e315c78b6c2d
cojmar <cojmar@gmail.com>
2026-03-11 07:46:00 +0200

    chaotic fix

--- install.sh
+++ install.sh
@@ -710,26 +710,17 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay-bin
 
-# install chaotic-aur
-sudo pacman -Sy --noconfirm git
-
+# install chaotic-aur repo correctly
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 
-git clone https://aur.archlinux.org/chaotic-keyring.git
-cd chaotic-keyring
-makepkg -si --noconfirm
-cd ..
-rm -rf chaotic-keyring
+sudo pacman -U --noconfirm \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst \
+https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
 
-git clone https://aur.archlinux.org/chaotic-mirrorlist.git
-cd chaotic-mirrorlist
-makepkg -si --noconfirm
-cd ..
-rm -rf chaotic-mirrorlist
-
-sudo sh -c "echo \"[chaotic-aur]\" >> /etc/pacman.conf"
-sudo sh -c "echo \"Include = /etc/pacman.d/chaotic-mirrorlist\" >> /etc/pacman.conf"
+sudo sh -c "echo '' >> /etc/pacman.conf"
+sudo sh -c "echo '[chaotic-aur]' >> /etc/pacman.conf"
+sudo sh -c "echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf"
 
 sudo pacman -Sy --noconfirm
 
