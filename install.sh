021ab3ee09c1b3f3a26e8b5735f0dca4bd3f4790
cojmar <cojmar@gmail.com>
2026-03-11 08:09:20 +0200

    fix aur

--- install.sh
+++ install.sh
@@ -709,23 +709,6 @@ cd yay-bin
 makepkg -si --noconfirm
 cd ..
 rm -rf yay-bin
-
-# install chaotic-aur repo correctly
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-
-sudo pacman -U --noconfirm \
-https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst \
-https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
-
-sudo sh -c "echo '' >> /etc/pacman.conf"
-sudo sh -c "echo '[chaotic-aur]' >> /etc/pacman.conf"
-sudo sh -c "echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf"
-
-sudo pacman -Sy --noconfirm
-
-# yay usage
-yay --noconfirm
 yay -Syu --noconfirm pacseek "${my_extra[@]}"
 yay -Yc --noconfirm
 '
