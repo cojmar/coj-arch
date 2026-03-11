fc4776f715a54ce904c6b9a888126c6a8fa6b62b
cojmar <cojmar@gmail.com>
2026-03-11 08:25:34 +0200

    chaotic-aur?

--- install.sh
+++ install.sh
@@ -700,6 +700,22 @@ echo $sep
 echo ================= INSTALLING AUR
 echo $sep
 
+
+arch-chroot /mnt /bin/sh -c '
+if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
+    pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+    pacman-key --lsign-key 3056513887B78AEB
+
+    pacman -U --noconfirm \
+    https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst \
+    https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
+
+    printf "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" >> /etc/pacman.conf
+
+    pacman -Sy --noconfirm
+fi
+'
+
 arch-chroot -u $my_user /mnt /bin/sh -c '
 cd ~
 
